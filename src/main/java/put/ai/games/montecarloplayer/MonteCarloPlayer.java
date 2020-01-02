d3f5c12060b15d3e85e7042b859b98a0267021e8
Marcin Zatorski <mar3211233@gmail.com>
2020-01-02 02:32:49 +0100

    Implement Monte Carlo Tree Search

--- src/main/java/put/ai/games/montecarloplayer/MonteCarloPlayer.java
+++ src/main/java/put/ai/games/montecarloplayer/MonteCarloPlayer.java
@@ -1,11 +1,11 @@
 package put.ai.games.montecarloplayer;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import put.ai.games.game.Board;
 import put.ai.games.game.Move;
 import put.ai.games.game.Player;
+
+import java.util.List;
+import java.util.Random;
 
 public class MonteCarloPlayer extends Player {
 
@@ -16,46 +16,74 @@ public class MonteCarloPlayer extends Player {
         return "Marcin Zatorski 136834 Sebastian Michoń 136770";
     }
 
-    public class Result {
+    public static class Node {
         public int wins = 0;
         public int games = 0;
+        public Node[] children;
     }
 
     @Override
     public Move nextMove(Board board) {
-        long timeLimit = getTime() - 10;
+        long timeLimit = getTime() - 500;
         long startTime = System.currentTimeMillis();
 
+        Node root = new Node();
         List<Move> moves = board.getMovesFor(getColor());
-        List<Result> results = new ArrayList<>();
-        for (int i = 0; i < moves.size(); ++i) {
-            results.add(new Result());
-        }
 
+        int simulationNum = 0;
         while (System.currentTimeMillis() - startTime < timeLimit) {
-            int nextMoveIndex = random.nextInt(moves.size());
-
-            board.doMove(moves.get(nextMoveIndex));
-            Color result = simulate(board, getOpponent(getColor()));
-            results.get(nextMoveIndex).games += 2;
-            if (result == getColor()) {
-                results.get(nextMoveIndex).wins += 2;
-            }
-            if (result == Color.EMPTY) {
-                results.get(nextMoveIndex).wins += 1;
-            }
-            board.undoMove(moves.get(nextMoveIndex));
+            select(root, board, getColor());
+            ++simulationNum;
         }
+        System.out.println(simulationNum);
 
-        int bestMoveIndex = 0;
-        double max = 0;
-        for (int i = 0; i < results.size(); ++i) {
-            if (max < (double) results.get(i).wins / results.get(i).games) {
-                max = (double) results.get(i).wins / results.get(i).games;
-                bestMoveIndex = i;
-            }
-        }
+        int bestMoveIndex = getBestMove(root);
+        Node bestNode = root.children[bestMoveIndex];
+        System.out.println("Best move index: " + bestMoveIndex);
+        System.out.format("W/G/R: %.1f %d %f\n", bestNode.wins / 2.0, bestNode.games,
+                (double) bestNode.wins / (2 * bestNode.games));
         return moves.get(bestMoveIndex);
+    }
+
+    public Color select(Node node, Board board, Color player) {
+        Color winner = board.getWinner(player);
+        if (winner != null) {
+            ++node.games;
+            if (winner == getOpponent(player)) {
+                node.wins += 2;
+            }
+            else if (winner == Color.EMPTY) {
+                ++node.wins;
+            }
+            return winner;
+        }
+
+        Color result;
+        if (node.children != null) {
+            int nextMoveIndex = getNextMove(node);
+            Move move = board.getMovesFor(player).get(nextMoveIndex);
+
+            board.doMove(move);
+            result = select(node.children[nextMoveIndex], board, getOpponent(player));
+            board.undoMove(move);
+        }
+        else {
+            node.children = new Node[board.getMovesFor(player).size()];
+            for (int i = 0; i < node.children.length; ++i) {
+                node.children[i] = new Node();
+            }
+            result = simulate(board, player);
+        }
+
+        ++node.games;
+        if (result == getOpponent(player)) {
+            node.wins += 2;
+        }
+        else if (result == Color.EMPTY) {
+            ++node.wins;
+        }
+
+        return result;
     }
 
     public Color simulate(Board b, Color player) {
@@ -71,5 +99,41 @@ public class MonteCarloPlayer extends Player {
         b.undoMove(nextMove);
 
         return result;
+    }
+
+    public int getNextMove(Node node) {
+        int nextMove = 0;
+        double maxScore = 0;
+        for (int i = 0; i < node.children.length; ++i) {
+            if (node.children[i].games == 0) {
+                return random.nextInt(node.children.length);
+            }
+            double score = uctScore(node.children[i].wins, node.children[i].games, node.games);
+            if (maxScore < score) {
+                maxScore = score;
+                nextMove = i;
+            }
+        }
+        return nextMove;
+    }
+
+    public double uctScore(int wins, int games, int totalGames) {
+        double C = 1.4;
+        return (double) wins / (2 * games) + C * Math.sqrt(Math.log(totalGames) / games);
+    }
+
+    public int getBestMove(Node node) {
+        int bestMove = 0;
+        double maxWinRatio = 0;
+        for (int i = 0; i < node.children.length; ++i) {
+            if (node.children[i].games != 0) {
+                double winRatio = (double) node.children[i].wins / node.children[i].games;
+                if (maxWinRatio < winRatio) {
+                    maxWinRatio = winRatio;
+                    bestMove = i;
+                }
+            }
+        }
+        return bestMove;
     }
 }
